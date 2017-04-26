2814ccc4846628135314890410351931aff6e0ea
Aidan <hacketta.t@gmail.com>
2017-04-26 21:03:49 +0100

    Character on screen

--- Centipede.c
+++ Centipede.c
@@ -5,12 +5,15 @@
     
     About: Centipede is a fixed shooter arcade game produced by Atari in 1980.
            This is a recreation of the game in C using the ncurses library.
+           
            To play, simply compile and run this file.
- */
+*/
  
 #include <ncurses.h>
 #include <unistd.h>
 
+
+WINDOW * create_newwin(int height, int width, int starty, int startx);
 
 int main(void)
 {
@@ -19,15 +22,40 @@ int main(void)
     int direction = 1; // Initialise movement to the left
     int cXPos, cYPos = 0; // Begin at the top left corner of the screen
     int yMax, xMax = 0; // The edges of the terminal window
-    int xNext = 0; // Next xposition for collision detection
+    int xNext = 0; // Next x position for collision detection
     
+    // Variables for the user character
+    int xPos;
+    int yPos;
+    int ch;
 
     initscr();
-    noecho();
-    curs_set(FALSE);
+    keypad(stdscr, TRUE);
+    noecho();  
+    
+    printw("Press x to exit");
+    refresh(); 
+   
+    
+    while(true)
+    {
+            getmaxyx(stdscr, yMax, xMax);
+            xPos = xMax / 2;
+            yPos = yMax - 1;
+            
+     
+  //   while((ch = getch()) != '       
+    //        case KEY_LEFT:
+            
+            clear();
+            mvprintw(yPos, xPos, "<O>");
+            refresh();
+            usleep(30000);  
+    }
+
     
     
-    //  Loop to make centipede descend 
+    /*  Loop to make centipede descend 
         while(true)
         {
             getmaxyx(stdscr, yMax, xMax);
@@ -48,8 +76,17 @@ int main(void)
             {
                 cXPos += direction;
             }            
-        } 
+        } */
         
     
         endwin();
 }
+
+
+
+
+
+
+
+
+
