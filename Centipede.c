044a912cfa2382ff85bec805a59f45433d74004e
Aidan <hacketta.t@gmail.com>
2017-04-26 20:33:58 +0100

    functional descending centipede. updated commenting & variable names

--- Centipede.c
+++ Centipede.c
@@ -11,26 +11,45 @@
 #include <ncurses.h>
 #include <unistd.h>
 
-#define DELAY 30000
 
 int main(void)
 {
-    int x, y = 0;
     
+    // Variables for x&y movement of Centipede
+    int direction = 1; // Initialise movement to the left
+    int cXPos, cYPos = 0; // Begin at the top left corner of the screen
+    int yMax, xMax = 0; // The edges of the terminal window
+    int xNext = 0; // Next xposition for collision detection
+    
+
     initscr();
     noecho();
     curs_set(FALSE);
     
-    while(true)
-    {
-        clear();
-        mvprintw(y, x, ")");
-        refresh();    
+    
+    //  Loop to make centipede descend 
+        while(true)
+        {
+            getmaxyx(stdscr, yMax, xMax);
+
+            clear();
+            mvprintw(cYPos, cXPos, "}@@@@@@@@{");  
+            refresh();   
+            usleep(30000);
+            
+            xNext = cXPos + direction;
+            
+            if (xNext >= xMax-9 || xNext < 0)
+            {
+                direction *= -1;
+                cYPos++;
+            }
+            else
+            {
+                cXPos += direction;
+            }            
+        } 
         
-        usleep(DELAY);   
-        x++;
-    }
-        
-        
+    
         endwin();
 }
